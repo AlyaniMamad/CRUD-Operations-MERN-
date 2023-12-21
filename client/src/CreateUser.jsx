c705134915a28b5fa4df6605de316e619408d694
Alyani Mamad <97268754+AlyaniMamad@users.noreply.github.com>
2023-12-21 10:32:22 +0530

    Connected db with server

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const CreateUser = () => {
+
+    const [name,setName] = useState()
+    const [email,setEmail] = useState()
+    const [age,setAge] = useState()
+
+    const Submit = (e) =>{
+        e.preventDefault();
+        axios.post("http://localhost:3001/createUser",{name,email,age})
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
-            <form>
+            <form onSubmit={Submit}>
                 <h2>Add User</h2>
                 <div className='mb-2'>
                     <label htmlFor=''>Name</label>
-                    <input type='text' placeholder='Enter Name...' className='form-control' required/>
+                    <input type='text' placeholder='Enter Name...' className='form-control' required
+                        onChange={(e)=>setName(e.target.value)}
+                    />
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Email</label>
-                    <input type='email' placeholder='Enter Email...' className='form-control' required/>
+                    <input type='email' placeholder='Enter Email...' className='form-control' required
+                        onChange={(e)=>setEmail(e.target.value)}
+                    />
                 </div>
                 <div className='mb-2'>
                     <label htmlFor=''>Name</label>
-                    <input type='number' placeholder='Enter Age...' className='form-control' min={1} max={150} required/>
+                    <input type='number' placeholder='Enter Age...' className='form-control' min={1} max={150} required
+                        onChange={(e)=>setAge(e.target.value)}
+                    />
                 </div>
                 <button className='btn btn-success'>Submit</button>
             </form>
